450d64d5572a1cc122368839a30771fd4638a5cd
Macarena-Chang <changmacarena@gmail.com>
2022-02-09 00:17:48 -0300

    Anotaciones para scan de componentes y entidades en clase main

--- src/main/java/inicio/MicroservicioVuelosApplication.java
+++ src/main/java/inicio/MicroservicioVuelosApplication.java
@@ -2,7 +2,12 @@ package inicio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+@EntityScan(basePackages = {"model"})
+@EnableJpaRepositories(basePackages = {"dao"})
+@ComponentScan(basePackages = {"controller","service","dao"})
 @SpringBootApplication
 public class MicroservicioVuelosApplication {
 
